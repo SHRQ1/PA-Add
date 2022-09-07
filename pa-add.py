3ae7a9ff3c1078d8ed8db6c4d03a139888619010
SHRQ1 <sheeroolt@gmail.com>
2022-09-07 12:49:44 +0300

    Initial commit

---
+++ pa-add.py
@@ -0,0 +1,117 @@
+from panos.firewall import Firewall
+from panos.objects import AddressObject
+import re
+from urllib.parse import urlparse
+import argparse
+import getpass
+
+
+
+
+
+server_ip='192.168.1.1'
+username='admin' # Change it to your username
+Tag='Test'
+
+ipAddr = []
+FDQN= []
+
+
+def classify(file_path):
+    with open(file_path) as f:
+        lines = f.readlines()
+    f.close()
+
+
+
+
+    Ip_pattern = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
+    FDQN_pattern = re.compile(r"^([a-zA-Z0-9._-]+$)")
+
+
+
+    for y in lines:
+        if y.isspace(): # skip empty line
+            continue
+        k = Ip_pattern.search(y)  # find Ip match the accepted pattern
+        if not k == None:
+                ipAddr.append(k.group())
+        else:
+            k = FDQN_pattern.search(y)  # find url match the accepted pattern
+            if not k==None:
+                FDQN.append(k.group())
+            else: # url not matching
+               y=parseFQDN(y)
+               if not y=='None':
+                 FDQN.append(y)
+
+                ########################################33
+
+def parseFQDN(url):
+    if urlparse(url).scheme:
+       return urlparse(url).hostname
+    elif '://' not in url:
+        url=url.replace('//','://')
+    else:
+        url = 'http://'+url
+
+    url = re.sub(r"[^A-Za-z0-9.-_]","",str(urlparse(url).hostname))
+    return url
+
+
+
+def add(input_desc,pswd):
+    try:
+        fw = Firewall(server_ip, username, pswd)
+        fw.show_system_info()
+    except:
+        print("Invalid credential")
+        exit(1)
+
+    print("\nThe script found the following FDQN :\n")
+    for x in FDQN:
+        print(x)
+
+    print("The script found the following Ip addresses :\n ")
+    for x in ipAddr:
+        print(x)
+
+    ################## Adding start here
+    for x in FDQN:
+        try:
+            obj = AddressObject(x, x, 'fqdn', input_desc, Tag)
+            fw.add(obj)
+            obj.create()
+        except:
+            print("Something went wrong with "+x)
+
+    for x in ipAddr:
+        try:
+            obj = AddressObject(x, x + '/32', 'ip-netmask', input_desc, Tag)
+            fw.add(obj)
+            obj.create()
+        except:
+            print("Something went wrong with "+x)
+
+    fw.commit()
+
+
+def main(file_path, input_desc,pswd):
+    classify(file_path)
+    add(input_desc,pswd)
+    print("the script done successfully ")
+
+
+if __name__ == '__main__':
+    pswd =getpass.getpass('Password:')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', type=str, help=" file path")
+    parser.add_argument("Description", type=str, help="description ")
+
+    args = parser.parse_args()
+    file_path = args.file
+    input_desc = args.Description
+
+    main(file_path, input_desc,pswd)
+
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PA-Add',
+    version='1.0',
+    packages=['panos','urllib'],
+    url='',
+    license='GNU General Public License v3.0 ',
+    author='Shorouq Altamimi',
+    author_email='shrq.tamimi@gmail.com',
+    description='Adding addresses for Palo Alto firewall from txt file.'
+)
